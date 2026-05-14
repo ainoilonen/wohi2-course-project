bc748db9db8bb37c8e0c7a8c30f3d6696e090958
Aino Ilonen <aino.ilonen@gmail.com>
2026-05-14 23:29:50 +0300

    commented one line

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -2,6 +2,6 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-console.log("DATABASE_URL =", process.env.DATABASE_URL);
+//console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
 module.exports = prisma;
